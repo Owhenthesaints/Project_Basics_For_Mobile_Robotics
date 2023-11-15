48e32fb9afaae98f15424d4593ac6dfb9ce5a995
Owhenthesaints <owen.simon@epfl.ch>
2023-11-15 23:20:26 +0100

    made initial argument non mutable for init pos

--- thymio_movement/ThymioRobot.py
+++ thymio_movement/ThymioRobot.py
@@ -3,7 +3,9 @@ from dependencies.AsyncClientInterface import AsyncClientInterface
 
 class ThymioRobot():
 
-    def __init__(self, init_position: list[int, int]= [0, 0]):
+    def __init__(self, init_position=None):
+        if init_position is None:
+            init_position = [0, 0]
         self.asyncClient = AsyncClientInterface()
         self._position = init_position
 
