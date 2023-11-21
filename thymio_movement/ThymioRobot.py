66060ce8db88b6db052fb0832d7f09e3f2bbc658
Owhenthesaints <owen.simon@epfl.ch>
2023-11-21 18:32:13 +0100

    made a new setter for position

--- thymio_movement/ThymioRobot.py
+++ thymio_movement/ThymioRobot.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 
 from dependencies.AsyncClientInterface import AsyncClientInterface
@@ -12,7 +10,7 @@ class ThymioRobot():
     movement_thread = None
     goal = None
     __stop_thread = False
-
+    __WHEEL_DISTANCE = 0.09  # in m
     AsyncClient = AsyncClientInterface()
     __TURN_FORWARD_RATE_SPEED_CONSTANT = 150
     on_objective: bool = False
@@ -22,30 +20,31 @@ class ThymioRobot():
                  secs_to_m=0.015):
         self.__CONVERSION_CONSTANT_TTT = time_to_turn_const
         if init_position is None:
-            self.position = np.array([0, 0, init_angle])
+            self._position = np.array([0, 0, init_angle])
         else:
-            self.position = np.concatenate((np.array(init_position), np.array(init_angle)))
+            self._position = np.concatenate((np.array(init_position), np.array(init_angle)))
         self.__SECS_TO_M = secs_to_m
         self.__THRESHOLD = threshold
         self.__KAPPA_ALPHA = KAPPA_ALPHA
         self.__KAPPA_BETA = KAPPA_BETA
         self.__KAPPA_RHO = KAPPA_RHO
 
-
     def set_new_goal(self, new_goal: list[int, int, int] | np.array):
         self.goal = np.array(new_goal)
 
+    def set_new_position(self, position: np.array):
+        self._position=position
+
     def apply_motor_command(self):
-        movement_vector = self.position - self.goal
+        movement_vector = self._position - self.goal
         rho = np.sqrt(movement_vector[1] ** 2 + movement_vector[0] ** 2)
         if rho < self.__THRESHOLD:
             self.on_objective = True
             return
-        alpha = -self.position[2] - self.goal[2]
+        alpha = -self._position[2] - self.goal[2]
         beta = - self.goal[2]
         forward_speed = self.__KAPPA_RHO * rho
-        turning_velocity = self.__KAPPA_ALPHA * alpha + self.__KAPPA_BETA * beta
+        turning_velocity = self.__WHEEL_DISTANCE / 2 * (self.__KAPPA_ALPHA * alpha + self.__KAPPA_BETA * beta)
         movement_array = [-turning_velocity + forward_speed, turning_velocity + forward_speed]
         self.AsyncClient.set_motors(left_motor=movement_array[0], right_motor=movement_array[1])
         self.on_objective = False
-
