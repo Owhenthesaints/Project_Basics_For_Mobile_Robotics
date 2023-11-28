95565ad18666ec622a7c3858fcf40ac480879dca
Owhenthesaints <owen.simon@epfl.ch>
2023-11-28 23:10:27 +0100

    made GlobalNav2 first draft

--- dependencies/helper_functions.py
+++ dependencies/helper_functions.py
@@ -1,6 +1,11 @@
 # limit the angle to (-pi, pi)
+import numpy as np
 def convert_angle(angle):
     angle = angle % (2*np.pi)
     if angle >= np.pi:
         angle -= 2*np.pi
     return angle
+
+
+class NoThymioError(Exception):
+    pass
--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 import numpy as np
-import time
+
 import dependencies.constants_robot as constants_robot
-from thymio_movement.ThymioRobot import ThymioRobot
-from kalman_filter.kalman import kalman_filter
+from dependencies.helper_functions import NoThymioError
 from kalman_filter.EKF import ExtendedKalmanFilter
-import thymio_movement.local_navigation as local
-from global_navigation_function import *
+from kalman_filter.kalman import kalman_filter
+from thymio_movement.ThymioRobot import ThymioRobot
+from global_navigation.GlobalNav2 import *
 
 DO_KALMAN = True
 LOCAL_AVOIDANCE = True
 VISION = True
 
-goal_position = [1000, 1000, np.pi/2]
-position = [0, 0, np.pi/2]
+if __name__ == "__main__":
+    # CAMERA INITIALISATION
+    CAMERA_ID = 1
+    video_stream, QR_detector = init_camera_QRdetector(1)
+    obstacle_vertices, goal_center, transformation_matrix = init_background(video_stream)
+    recalculate_global = True
+    last_robot_center = None
+    windowName = "Tracking"
 
+    # initialize vision
+    position = [0, 0, np.pi / 2]
 
-if __name__=="__main__":
-    #initialize vision
-    if VISION:
-        ...
-                
-    
-    
     if DO_KALMAN:
         if position is None:
-            KF = ExtendedKalmanFilter(position)
-        else:
-            print("Thymio has not been found")
+            raise NoThymioError("no thymio found for initialisation of Kalman Filter")
+        KF = ExtendedKalmanFilter(position)
 
     little_thymio = ThymioRobot()
+
+
+
+
+    #main Loop
     while little_thymio.is_alive:
+
+
+
         if np.any(little_thymio.get_sensors() > constants_robot.LOCAL_NAV_SWITCH):
-             little_thymio.local_nav()
+            little_thymio.local_nav()
         # DO_Kalman initialisation
+        if VISION:
+
+            position = None
 
         # apply kalman
         if DO_KALMAN:
@@ -44,5 +55,3 @@ if __name__=="__main__":
         # here make final condition if thymio ends up on the final point
         if little_thymio.on_objective:
             little_thymio.kill()
-
-
